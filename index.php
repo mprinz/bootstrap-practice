a8a85ab8e68ed8fe8959ad9059dde0176c7fa536
Michael Prinz <mnprinz@gmail.com>
2016-01-27 20:37:12 -0700

    added index file

--- index.php
+++ index.php
@@ -1,39 +1,50 @@
 <!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8">
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
 
 
-			<!-- Latest compiled and minified CSS -->
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+				integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-			<!-- Optional theme -->
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+				integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-			<!-- Latest compiled and minified JavaScript -->
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-			<link rel="stylesheet" href="style.css">
-			<title>Bootstrap practice</title>
-		</head>
-		<body>
-			<div class="container">
-				  <div class="row">
-					  <div class="row">
-						  <div class="col-md-11">
-							  Level 1: .col-md-11
-					  <div class="col-sm-2">.col-sm-2</div>
-					  <div class="col-sm-4 col-sm-offset-4">.col-sm-4 .col-sm-offset-4</div>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+				  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+				  crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="style.css">
+		<title>Bootstrap practice</title>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">.col-md-12</div>
+					Level 1: .col-md-12
+					<div class="row">
+						<div class="col-sm-2">.col-sm-2</div>
+							Level 2: .col-sm-2
+							<p>Hello World!</p>
+							</div>
 					</div>
-			</div>
-				  </div>
-			</div>
-
-			<div class="container">
-				<div class="row">
-					<div class=""
 				</div>
 			</div>
-		</body>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-2">.col-md-10.col-md-offset-2</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-md-offset-2">.col-md-4.col-md-offset-2</div>
+				<div class="col-md-4 col-md-offset-2">.col-md-4.col-md-offset-2</div>
+			</div>
+		</div>
+	</body>
 
 
 </html>
