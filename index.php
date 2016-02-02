e80ac85ffc9255510b58c8de0c4c6e7fda700b37
Michael Prinz <mnprinz@gmail.com>
2016-02-02 13:11:55 -0700

    edited bootstrap

--- index.php
+++ index.php
@@ -105,6 +105,7 @@
 							<div class="row">
 								<div class="col-md-4">
 									<div class="content-box">
+										<h4>This is a box</h4>
 										<p>Ipsum loquator cucumber airplane chairlift university</p>
 									</div>
 								</div>
@@ -118,7 +119,7 @@
 										<p>Four score and seven years ago our forefathers...</p>
 									</div>
 								</div>
-						<div class="container-fluid">
+						<div class="container">
 							<div class="row">
 								<div class="col-md-12">
 									<div class="jumbotron"></div>
