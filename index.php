6105d305dd8084854906ae91d3e62b04c049305c
Michael Prinz <mnprinz@gmail.com>
2016-02-02 11:03:54 -0700

    edited bootstrap

--- index.php
+++ index.php
@@ -16,34 +16,116 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
 				  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
 				  crossorigin="anonymous"></script>
-		<link rel="stylesheet" href="style.css">
+		<link type="text/css" rel="stylesheet" href="style.css">
 		<title>Bootstrap practice</title>
 	</head>
-	<body>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">.col-md-12</div>
-					Level 1: .col-md-12
-					<div class="row">
-						<div class="col-sm-2">.col-sm-2</div>
-							Level 2: .col-sm-2
-							<p>Hello World!</p>
+		<body class="sfooter">
+			<div class="sfooter-content">
+				<header>
+					<nav class="navbar navbar-inverse">
+							<div class="container">
+								<!-- Brand and toggle get grouped for better mobile display -->
+								<div class="navbar-header">
+									<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-nav" aria-expanded="false">
+										<span class="sr-only">Toggle navigation</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+									<a class="navbar-brand" href="#">Prinzable Web Design</a>
+								</div>
+
+								<!-- Collect the nav links, forms, and other content for toggling -->
+								<div class="collapse navbar-collapse" id="top-nav">
+									<ul class="nav navbar-nav">
+										<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+										<li><a href="#">Link</a></li>
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+											<ul class="dropdown-menu">
+												<li><a href="#">Action</a></li>
+												<li><a href="#">Another action</a></li>
+												<li><a href="#">Something else here</a></li>
+												<li role="separator" class="divider"></li>
+												<li><a href="#">Separated link</a></li>
+												<li role="separator" class="divider"></li>
+												<li><a href="#">One more separated link</a></li>
+											</ul>
+										</li>
+									</ul>
+									<form class="navbar-form navbar-left" role="search">
+										<div class="form-group">
+											<input type="text" class="form-control" placeholder="Search">
+										</div>
+										<button type="submit" class="btn btn-default">Submit</button>
+									</form>
+									<ul class="nav navbar-nav navbar-right">
+										<li><a href="#">Link</a></li>
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+											<ul class="dropdown-menu">
+												<li><a href="#">Link 1</a></li>
+												<li><a href="#">Link 2</a></li>
+												<li><a href="#">Link 3</a></li>
+												<li role="separator" class="divider"></li>
+												<li><a href="#">Separated link</a></li>
+											</ul>
+										</li>
+									</ul>
+								</div><!-- /.navbar-collapse -->
+							</div><!-- /.container -->
+						</nav>
+					<div id="welcome">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="jumbotron">
+										<h1> Michael Prinz</h1>
+										<p>Web developer for hire.</p>
+										<span class="glyphicon glyphicon-king" aria-hidden="true"></span>
+									</div>
+								</div>
 							</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-10 col-md-offset-2">.col-md-10.col-md-offset-2</div>
-			</div>
-		</div>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4 col-md-offset-2">.col-md-4.col-md-offset-2</div>
-				<div class="col-md-4 col-md-offset-2">.col-md-4.col-md-offset-2</div>
-			</div>
-		</div>
+							<!-----Row 2---->
+							<div class="row">
+								<div class="col-md-6">
+									<div class="content-box">
+										<h3>Text goes here</h3>
+										<p>These are the voyages of the Starship Enterprise</p>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="content-box">
+										<h3>About Michael</h3>
+										<p>Michael is a human living on planet Earth but he is from planet Xylitol</p>
+									</div>
+								</div>
+							</div>
+							<!---Row 3---->
+							<div class="row">
+								<div class="col-md-4">
+									<div class="content-box">
+										<p>Ipsum loquator cucumber airplane chairlift university</p>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="content-box">
+										<p>Four score and seven years ago our forefathers...</p>
+							</div>
+						</div>
+								<div class="col-md-4">
+									<div class="content-box">
+										<p>Four score and seven years ago our forefathers...</p>
+									</div>
+								</div>
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="jumbotron"></div>
+									</div>
+								</div>
+							</div>
+
 	</body>
 
 
