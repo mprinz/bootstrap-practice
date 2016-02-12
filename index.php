19a1a4a3f77cd5c30f66daa4da66f450a32cb4b7
Michael Prinz <mnprinz@gmail.com>
2016-02-11 19:32:50 -0700

    added carousel code from http://getbootstrap.com/javascript/#carousel

--- index.php
+++ index.php
@@ -22,7 +22,42 @@
 		<body class="sfooter">
 			<div class="sfooter-content">
 				<header>
-					<nav class="navbar navbar-inverse">
+					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+							<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+							<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+						</ol>
+
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner" role="listbox">
+							<div class="item active">
+								<img src="images/IMG_1431.JPG" alt="photo height = 50 width= 100 ">
+								<div class="carousel-caption">
+									...
+								</div>
+							</div>
+							<div class="item">
+								<img src="..." alt="...">
+								<div class="carousel-caption">
+									...
+								</div>
+							</div>
+							...
+						</div>
+
+						<!-- Controls -->
+						<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+					<!--nav class="navbar navbar-inverse">
 							<div class="container">
 								<!-- Brand and toggle get grouped for better mobile display -->
 								<div class="navbar-header">
@@ -79,14 +114,16 @@
 						<div class="container">
 							<div class="row">
 								<div class="col-md-12">
-									<div class="jumbotron">
-										<h1> Michael Prinz</h1>
-										<p>Web developer for hire.</p>
-										<span class="glyphicon glyphicon-king" aria-hidden="true"></span>
+									<h1> Michael Prinz</h1>
+										<p>Web developer | Attorney | Photographer | Musician></p>
+									<a href ="#" class="btn btn-primary btn-large">Link</a>
+
 									</div>
 								</div>
 							</div>
-							<!-----Row 2---->
+						</div>
+						<!-----Row 2---->
+						<div class ="container>
 							<div class="row">
 								<div class="col-md-6">
 									<div class="content-box">
