1d14f367254d4c2d4d99f5cc88c96adcdf4b9b08
Michael Prinz <mnprinz@gmail.com>
2016-02-11 20:46:12 -0700

    added carousel code from http://getbootstrap.com/javascript/#carousel

--- index.php
+++ index.php
@@ -19,33 +19,28 @@
 		<link type="text/css" rel="stylesheet" href="style.css">
 		<title>Bootstrap practice</title>
 	</head>
-		<body class="sfooter">
-			<div class="sfooter-content">
-				<header>
+		<body>
+			<main>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
 					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 						<!-- Indicators -->
 						<ol class="carousel-indicators">
 							<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
 							<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-							<li data-target="#carousel-example-generic" data-slide-to="2"></li>
 						</ol>
 
 						<!-- Wrapper for slides -->
 						<div class="carousel-inner" role="listbox">
 							<div class="item active">
-								<img src="images/IMG_1431.JPG" alt="photo height = 50 width= 100 ">
-								<div class="carousel-caption">
-									...
+								<img src="images/IMG_1431.JPG" alt="photo">
 								</div>
-							</div>
 							<div class="item">
-								<img src="..." alt="...">
-								<div class="carousel-caption">
-									...
+								<img src="images/Sculpture.JPG" alt="photo1">
 								</div>
 							</div>
-							...
-						</div>
+					</div>
 
 						<!-- Controls -->
 						<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
@@ -57,59 +52,6 @@
 							<span class="sr-only">Next</span>
 						</a>
 					</div>
-					<!--nav class="navbar navbar-inverse">
-							<div class="container">
-								<!-- Brand and toggle get grouped for better mobile display -->
-								<div class="navbar-header">
-									<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-nav" aria-expanded="false">
-										<span class="sr-only">Toggle navigation</span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-									</button>
-									<a class="navbar-brand" href="#">Prinzable Web Design</a>
-								</div>
-
-								<!-- Collect the nav links, forms, and other content for toggling -->
-								<div class="collapse navbar-collapse" id="top-nav">
-									<ul class="nav navbar-nav">
-										<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-										<li><a href="#">Link</a></li>
-										<li class="dropdown">
-											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-											<ul class="dropdown-menu">
-												<li><a href="#">Action</a></li>
-												<li><a href="#">Another action</a></li>
-												<li><a href="#">Something else here</a></li>
-												<li role="separator" class="divider"></li>
-												<li><a href="#">Separated link</a></li>
-												<li role="separator" class="divider"></li>
-												<li><a href="#">One more separated link</a></li>
-											</ul>
-										</li>
-									</ul>
-									<form class="navbar-form navbar-left" role="search">
-										<div class="form-group">
-											<input type="text" class="form-control" placeholder="Search">
-										</div>
-										<button type="submit" class="btn btn-default">Submit</button>
-									</form>
-									<ul class="nav navbar-nav navbar-right">
-										<li><a href="#">Link</a></li>
-										<li class="dropdown">
-											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-											<ul class="dropdown-menu">
-												<li><a href="#">Link 1</a></li>
-												<li><a href="#">Link 2</a></li>
-												<li><a href="#">Link 3</a></li>
-												<li role="separator" class="divider"></li>
-												<li><a href="#">Separated link</a></li>
-											</ul>
-										</li>
-									</ul>
-								</div><!-- /.navbar-collapse -->
-							</div><!-- /.container -->
-						</nav>
 					<div id="welcome">
 						<div class="container">
 							<div class="row">
@@ -163,7 +105,7 @@
 									</div>
 								</div>
 							</div>
-
+								</main>
 	</body>
 
 
