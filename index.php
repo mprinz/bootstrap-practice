62b11ac63f669f28678a02e536391cef66ccf53c
Michael Prinz <mnprinz@gmail.com>
2016-02-14 18:55:47 -0700

    edited carousel

--- index.php
+++ index.php
@@ -24,29 +24,29 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12">
-					<div id="carousel" class="carousel slide" data-ride="carousel">
+					<div id="mycarousel" class="carousel slide" data-ride="carousel">
 						<!-- Indicators -->
 						<ol class="carousel-indicators">
-							<li data-target="#carousel" data-slide-to="0" class="active"></li>
-							<li data-target="#carousel" data-slide-to="1"></li>
-							<li datat-target="#carousel" data-slide-to="2"></li>
+							<li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+							<li data-target="#mycarousel" data-slide-to="1"></li>
+							<li data-target="#mycarousel" data-slide-to="2"></li>
 						</ol>
 
 						<!-- Wrapper for slides -->
 						<div class="carousel-inner" role="listbox">
 							<div class="item active">
-								<img src="images/IMG_1431.JPG" alt="photo">
+								<img src="images/IMG_1431.JPG" alt="photo" class="img-responsive">
 								</div>
 							<div class="item">
-								<img src="images/Sculpture.JPG" alt="photo1">
+								<img src="images/Sculpture.JPG" alt="photo" class ="img-responsive">
 								</div>
+							<div class="item">
+								<img src="images/La%20Cienaga%20Petroglyph.JPG" alt="photo" class="img-responsive">
 							</div>
-							<div class ="item">
-								<img src="images/La%20Cienaga%20Petroglyph.JPG" alt="photo2">
 					</div>
 
 						<!-- Controls -->
-						<a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+						<a class="left carousel-control" href="#mycarousel" role="button" data-slide="prev">
 							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 							<span class="sr-only">Previous</span>
 						</a>
