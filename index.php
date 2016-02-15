fafde424be81d12a2db0805ba5e5bbfac451cf31
Michael Prinz <mnprinz@gmail.com>
2016-02-14 18:27:24 -0700

    edited carousel

--- index.php
+++ index.php
@@ -24,11 +24,12 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12">
-					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+					<div id="carousel" class="carousel slide" data-ride="carousel">
 						<!-- Indicators -->
 						<ol class="carousel-indicators">
-							<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-							<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+							<li data-target="#carousel" data-slide-to="0" class="active"></li>
+							<li data-target="#carousel" data-slide-to="1"></li>
+							<li datat-target="#carousel" data-slide-to="2"></li>
 						</ol>
 
 						<!-- Wrapper for slides -->
@@ -40,14 +41,16 @@
 								<img src="images/Sculpture.JPG" alt="photo1">
 								</div>
 							</div>
+							<div class ="item">
+								<img src="images/La%20Cienaga%20Petroglyph.JPG" alt="photo2">
 					</div>
 
 						<!-- Controls -->
-						<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+						<a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
 							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 							<span class="sr-only">Previous</span>
 						</a>
-						<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+						<a class="right carousel-control" href="#carousel" role="button" data-slide="next">
 							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 							<span class="sr-only">Next</span>
 						</a>
